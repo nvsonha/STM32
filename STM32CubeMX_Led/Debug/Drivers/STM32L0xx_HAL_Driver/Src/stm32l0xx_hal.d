d3cc033dd86bb99d30e77f3307e6d758866785aa
Ha <nvsonha@gmail.com>
2019-08-29 13:08:25 +0200

    Firt Commit - Blink LED

---
+++ STM32CubeMX_Led/Debug/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal.d
@@ -0,0 +1,80 @@
+Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal.o: \
+ ../Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal.c \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Inc/stm32l0xx_hal_conf.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Include/core_cm0plus.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h \
+ C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Inc/stm32l0xx_hal_conf.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Include/core_cm0plus.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h:
+
+C:/Users/Amazon/workspace/STM32CubeMX_Led/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h:
